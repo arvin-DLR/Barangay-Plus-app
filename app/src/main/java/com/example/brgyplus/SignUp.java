3b4d637b7209d371d9731f33b53bcaf61f300d95
gekkkota <82653369+gekkkota@users.noreply.github.com>
2021-06-16 18:27:40 +0800

    Update SignUp.java

--- app/src/main/java/com/example/brgyplus/SignUp.java
+++ app/src/main/java/com/example/brgyplus/SignUp.java
@@ -1,4 +1,14 @@
 package com.example.brgyplus;
 
-public class SignUp {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
 }
